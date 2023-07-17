5aaf53e0cc59de4bb2072b418b663e4d0f8ea124
imprasanth2000 <battiniprasanth2000@gmail.com>
2023-07-17 15:57:17 +0530

    routing by prasanth

---
+++ src/components/Manatee/Manatee.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Manatee() {
+  return <div style={{
+    fontFamily : "cursive",
+    fontPalette : "light",
+    fontSize : "small"
+  }}>Manatee</div>;
+}
